4df48559886a7306ec509ab204d88a2472ed3b09
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-06-30 12:24:29 -0500

    agregado rutas nav

--- proyecto-1/script/script.js
+++ proyecto-1/script/script.js
@@ -1,14 +1,8 @@
-// Apartado para imprimir un mensaje de prueba en la consola
-console.log("Hola mundo");
-
-
-
-
 
 // Apartado para descargar el hoja de vida  
 let openButton = document.getElementById("btn"); // Se crea una variable que guarda el botón con el ID "btn"
 openButton.addEventListener("click", function() { // Se agrega un Event Listener al botón que escucha un evento "click" y ejecuta la función que le sigue.
-  fetch("./archivos/Desarollador Jr - Jhan Carlos Acuña.pdf") // Se realiza una petición HTTP para obtener el archivo PDF ubicado en la ruta especificada
+  fetch("./archivos/CV Jhan Carlos Acuña Ordoñez.pdf") // Se realiza una petición HTTP para obtener el archivo PDF ubicado en la ruta especificada
     .then(response => response.arrayBuffer()) // Se convierte el contenido a un ArrayBuffer
     .then(buffer => { // Se recibe el ArrayBuffer resultante y se ejecuta la función que le sigue.
       let blob = new Blob([buffer], { type: "application/pdf" }); // Se crea un objeto Blob a partir del ArrayBuffer y se especifica el tipo de archivo como PDF
@@ -19,25 +13,6 @@ openButton.addEventListener("click", function() { // Se agrega un Event Listener
       a.click(); // Se simula un clic en el elemento "a", lo que hace que el archivo PDF se descargue automáticamente.
     });
 });
-
-
-// //Apartado para cambiar la imagen en diferentes tamaaños de pantallas
-// // Función para cambiar la imagen en función del tamaño de la pantalla
-// function cambiarImagen() {
-//   var anchoPantalla = window.innerWidth;
-//   var imagen = document.getElementById('imagen-personal');
-  
-//   if (anchoPantalla < 768) {
-//     imagen.src = './img/foto.jpeg';
-//   } else {
-//     imagen.src = './img/img-personal.png';
-//   }
-// }
-
-// Llamada inicial a la función para establecer la imagen correcta
-cambiarImagen();
-// Evento que se dispara cuando el tamaño de la pantalla cambia
-window.addEventListener('resize', cambiarImagen);
 
 
 
